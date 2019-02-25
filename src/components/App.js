1bdda85bee4b9242c5a2136f38520556377c1571
Eduardo L. Torres <eltorrescuesta@gmail.com>
2019-02-25 12:55:55 +0100

    add styles

--- src/components/App.js
+++ src/components/App.js
@@ -10,20 +10,23 @@ class App extends Component {
   addBook = book => {
     const books = { ...this.state.books };
     books[`book${Date.now()}`] = book;
-    // console.log(books);
     this.setState({ books });
   };
 
   render() {
     return (
-      <div>
-        <h1>Book Finder</h1>
-        <SearchInput addBook={this.addBook} />
-        <ul>
-          {Object.keys(this.state.books).map(key => (
-            <BookCard key={key} book={this.state.books[key]} />
-          ))}
-        </ul>
+      <div className="wrapper">
+        <header>
+          <h1>Book Finder</h1>
+          <SearchInput addBook={this.addBook} />
+        </header>
+        <div className="books-container">
+          <ul className="books">
+            {Object.keys(this.state.books).map(key => (
+              <BookCard key={key} book={this.state.books[key]} />
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
