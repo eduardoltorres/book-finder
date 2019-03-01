1368c2c8923309b7b685c45901c4095cbc22f9e6
Eduardo L. Torres <eltorrescuesta@gmail.com>
2019-03-01 11:52:11 +0100

    make pwa

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/style.css";
 import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+serviceWorker.register();
